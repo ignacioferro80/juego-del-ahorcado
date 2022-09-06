05135f69894171e9767c003ff227f5a1a6203eee
ignacioferro <nachoferrro112233@gmail.com>
2022-09-06 11:49:17 -0300

    addded actions to the buttons

--- ahorcado-script.js
+++ ahorcado-script.js
@@ -1,6 +1,36 @@
-const inputTexto = document.querySelector(".encryptInput");
+/*Buttons*/
+const addWordPage = document.querySelector(".addWordPage");
+const homePage    = document.querySelector(".homePage");
+const gamePage    = document.querySelector(".gamePage");
 
-const cancelButton = document.querySelector(".cancelButton");
+/*Go home button*/
+function goHomePage(){
+    homePage.style.display = "block";
+
+    addWordPage.style.display = "none";
+    gamePage.style.display = "none";
+}
+
+/*Go game button*/
+function goGamePage(){
+    gamePage.style.display = "block";
+
+    addWordPage.style.display = "none";
+    homePage.style.display = "none";
+}
+
+/*Go add word page button*/
+function goAddWordPage(){
+    addWordPage.style.display = "block";
+
+    gamePage.style.display = "none";
+    homePage.style.display = "none";
+}
+
+/*Clena input*/
+const inputTexto = document.querySelector(".encryptInput");
+const cleanButton = document.querySelector(".cleanButton");
+
 function cleanInput() {
     inputTexto.value="";
 }
