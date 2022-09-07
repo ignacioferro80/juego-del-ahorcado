68f05f2c237d45d6210d3ba6f85e0f368fd4e6c0
ignacioferro <nachoferrro112233@gmail.com>
2022-09-07 18:58:24 -0300

    abrí canvas y creé lista de palabras para agregar

--- ahorcado-script.js
+++ ahorcado-script.js
@@ -27,10 +27,51 @@ function goAddWordPage(){
     homePage.style.display = "none";
 }
 
-/*Clena input*/
+/*Clean input button*/
 const inputTexto = document.querySelector(".encryptInput");
 const cleanButton = document.querySelector(".cleanButton");
 
 function cleanInput() {
     inputTexto.value="";
 }
+
+/*Random list button*/
+function randomWordTest(){
+    alert(secretWordListAdded.sample());
+}
+
+/*Save word button*/
+function saveNewWord(){
+    if(inputTexto.value==""){
+        alert("Write a word to be used in the game!")
+    }
+
+    else {
+        add_ToArray_(inputTexto.value, secretWordListAdded)
+        inputTexto.value="";
+    }
+}
+
+function add_ToArray_(string, array){
+    array.push([string])
+}
+
+/*Secret word list*/
+Array.prototype.sample = function(){
+    return this[Math.floor(Math.random()*this.length)];
+}
+
+const secretWordListDefault = ["retro", "computer", "language", "search", "practice"];
+const secretWordListAdded = ["retro", "computer"];
+
+
+
+/*Game page*/
+
+/*Dibujo*/
+var pantalla = document.querySelector("canvas"); //Pasamos los datos del canvas a JS
+var pincel = pantalla.getContext("2d");
+
+    // pincel.fillStyle = "darkblue"; 
+    // pincel.fillRect(500,500,200,400);
+;
