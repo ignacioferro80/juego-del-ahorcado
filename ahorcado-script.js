98d3c4488f1d921c2853e6dadd4442f1fc22a65c
ignacioferro <nachoferrro112233@gmail.com>
2022-09-08 12:17:17 -0300

    created word verification and adding new words

--- ahorcado-script.js
+++ ahorcado-script.js
@@ -27,6 +27,11 @@ function goAddWordPage(){
     homePage.style.display = "none";
 }
 
+/*Reload page button*/
+function reloadPage(){
+    window.location.reload();
+}
+
 /*Clean input button*/
 const inputTexto = document.querySelector(".encryptInput");
 const cleanButton = document.querySelector(".cleanButton");
@@ -35,38 +40,66 @@ function cleanInput() {
     inputTexto.value="";
 }
 
-/*Random list button*/
-function randomWordTest(){
-    alert(secretWordListAdded.sample());
-}
 
 /*Save word button*/
+
+//original list: var secretWordListAdded = ["retro", "computer", "language", "search", "practice"];
+
+//practice list:
+var secretWordListAdded = ["retro", "computer"];
+
+//Show list in console for verification:
+console.log(secretWordListAdded)
+
 function saveNewWord(){
+    //This button saves the word written and adds it to the secretWordListAdded array. Otherwise it displays an alert.
     if(inputTexto.value==""){
         alert("Write a word to be used in the game!")
     }
 
     else {
-        add_ToArray_(inputTexto.value, secretWordListAdded)
+        secretWordListAdded = add_ToArray_(inputTexto.value, secretWordListAdded)
         inputTexto.value="";
     }
 }
 
 function add_ToArray_(string, array){
+    //adds the string given to the array given and returns the final array.
     array.push([string])
+
+    return(array)
 }
 
 /*Secret word list*/
+    //Takes an array and picks a random element from it to return it.
 Array.prototype.sample = function(){
     return this[Math.floor(Math.random()*this.length)];
 }
 
-const secretWordListDefault = ["retro", "computer", "language", "search", "practice"];
-const secretWordListAdded = ["retro", "computer"];
-
 
 
 /*Game page*/
+const keyboardInput = document.querySelector(".testGameInput")
+
+keyboardInput.addEventListener("keyup", e=>{
+    if(e.keyCode == 13){
+        checkWord(keyboardInput.value)
+        keyboardInput.value="";
+    }
+})
+
+function checkWord(string) {
+    //Given a string, it checks if its the same one as the randomly guessed in the array of words
+
+    if(string == secretWordListAdded.sample()){
+        alert("You guessed the word!")
+    }
+
+    else{
+        alert("You did not guess, keep trying!")
+    }
+
+}
 
 /*Dibujo*/
 var pantalla = document.querySelector("canvas"); //Pasamos los datos del canvas a JS
